a022fbc679029dc35c84bb83925e10ca9efc454b
ketank-new <ketan22584@gmail.com>
2017-11-07 08:07:06 +0000

    added changes to sles12-sp3

--- src/config/config.py
+++ src/config/config.py
@@ -41,7 +41,8 @@ SUPPORTED_DISTROS = {
     'SUSE Linux Enterprise Server': {
         '11 SP4': 'Suse_Linux_Enterprise_Server_11_SP4_Package_List.json',
         '12 SP1': 'Suse_Linux_Enterprise_Server_12_SP1_Package_List.json',
-        '12 SP2': 'Suse_Linux_Enterprise_Server_12_SP2_Package_List.json'
+        '12 SP2': 'Suse_Linux_Enterprise_Server_12_SP2_Package_List.json',
+        '12 SP3': 'Suse_Linux_Enterprise_Server_12_SP3_Package_List.json'
     },
     'SUSE Package Hub': {
         'SLES 12 SP1': 'SUSE_Package_Hub_SLES_12_SP1.json',
